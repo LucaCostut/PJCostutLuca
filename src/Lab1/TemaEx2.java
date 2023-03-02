5404293ab33ed10df8ca1a66a02915ce639bccd0
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2023-03-02 16:52:47 +0200

    commit

--- src/Lab1/TemaEx2.java
+++ src/Lab1/TemaEx2.java
@@ -1,17 +1,21 @@
 package Lab1;
 import java.io.*;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
+
 public class TemaEx2 {
     public static void main(String[] args) throws IOException {
         String nume_fis = "in.txt";
         BufferedReader flux_in;
-        flux_in = new BufferedReader(new InputStreamReader(new FileInputStream(nume_fis))) {
-        };
-        String linie = flux_in.readLine();
-        Scanner scanner = new Scanner(new File("in.txt"));
-        int x = scanner.nextInt();
-        System.out.println(x);
-
+        try {
+            flux_in = new BufferedReader(new InputStreamReader(new FileInputStream(nume_fis)));
+            String linie = null;
+            while ((linie = flux_in.readLine()) != null) {
+                System.out.println(linie);
+            }
+            flux_in.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Fisierul " + nume_fis + " nu a fost gasit.");
+        }
     }
 }
+
