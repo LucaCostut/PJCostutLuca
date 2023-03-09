3150ec41ae1dea28b187377f9c5c8f7ac9bf5333
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2023-03-09 18:27:51 +0200

    commit

---
+++ src/Lab1/Lab3Ex1.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class Lab3Ex1 {
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
---
+++ src/Lab1/Parabola.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+class Parabola {
+    private  int a;
+    private  int b;
+    private  int c;
+    public Parabola (int a,int b,int c)
+    {
+        super();
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+
+    
+}
