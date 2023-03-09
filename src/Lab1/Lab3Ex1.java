945d61d32fe883a3c4768f8be757168b3ce3c2af
LucaCostut <115777986+LucaCostut@users.noreply.github.com>
2023-03-09 19:35:37 +0200

    commit

--- src/Lab1/Lab3Ex1.java
+++ src/Lab1/Lab3Ex1.java
@@ -1,9 +1,20 @@
 package Lab1;
 
-public class Lab3Ex1 {
+import java.util.ArrayList;
+import java.util.List;
+
+class Lab3Ex1 {
     public static void main(String[] args)
     {
-        
+        List <Parabola> parabole =new ArrayList<Parabola>();
+        parabole.add(new Parabola(1, 2, 3));
+        parabole.add(new Parabola(3, 4, 5));
+        parabole.add(new Parabola(6, 7, 8));
+        parabole.add(new Parabola(9, 10, 11));
+        System.out.println("Mijloc:");
+        System.out.println(Parabola.mijloc(parabole.get(2), parabole.get(3)));
+        System.out.println("Lungime:");
+        System.out.println(Parabola.lungime(parabole.get(0), parabole.get(1)));
     }
     
 }
--- src/Lab1/Parabola.java
+++ src/Lab1/Parabola.java
@@ -1,10 +1,9 @@
 package Lab1;
-
 class Parabola {
-    private  int a;
-    private  int b;
-    private  int c;
-    public Parabola (int a,int b,int c)
+    private  double a;
+    private  double b;
+    private  double c;
+    public Parabola (double a,double b,double c)
     {
         super();
         this.a = a;
@@ -12,6 +11,46 @@ class Parabola {
         this.c = c;
 
     }
+    public String toString(){
+        return String.format("f(x) = %.2f x^2 + %.2f x + %.2f", a, b, c);
+    }
+    public static double[] mijloc (Parabola p1,Parabola p2)
+    {
+        double a1 = p1.a;
+        double b1 = p1.b;
+        double c1 = p1.c;
+        double a2 = p2.a;
+        double b2 = p2.b;
+        double c2 = p2.c;
 
-    
+
+        double x1 = -b1/(2*a1);
+        double x2 = -b2/(2*a2);
+        double y1 = -1 * (b1 * b1 - 4 * a1 * c1) / (4 * a1);
+        double y2 =  -1 * (b2 * b2 - 4 * a2 * c2) / (4 * a2);
+        
+        double xMij = (x1+x2)/2;
+        double yMij = (y1+y2)/2;
+        
+        double[] mijloc = {xMij,yMij};
+        
+        return mijloc;
+    }
+    public static double lungime (Parabola p1,Parabola p2)
+    {
+        double a1 = p1.a;
+        double b1 = p1.b;
+        double c1 = p1.c;
+        double a2 = p2.a;
+        double b2 = p2.b;
+        double c2 = p2.c;
+
+
+        double x1 = -b1/(2*a1);
+        double x2 = -b2/(2*a2);
+        double y1 = -1 * (b1 * b1 - 4 * a1 * c1) / (4 * a1);
+        double y2 =  -1 * (b2 * b2 - 4 * a2 * c2) / (4 * a2);
+        double distance =(double) Math.hypot(x2 - x1, y2 - y1);
+        return distance;
+    }
 }
